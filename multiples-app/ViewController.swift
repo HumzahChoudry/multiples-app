585b97f346419a594bc71fa69cea00a9cd3f6296
Humzah Choudry <humzahchoudry@Humzahs-MacBook-Pro.local>
2017-05-31 17:36:18 -0500

    finished app v1

--- multiples-app/ViewController.swift
+++ multiples-app/ViewController.swift
@@ -20,6 +20,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    var multipleNum = 0
+    var result = 0
+    var previousResult = 0
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var whatMultipleTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var equationTxt: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+        if whatMultipleTxt.text != nil && whatMultipleTxt.text != ""
+        {
+            multipleNum = Int(whatMultipleTxt.text!)!
+            
+            logoImg.isHidden = true
+            whatMultipleTxt.isHidden = true
+            playBtn.isHidden = true
+            addBtn.isHidden = false
+            equationTxt.isHidden = false
+            
+            equationTxt.text = "You chose \(multipleNum) you douuuuche"
+        }
+    }
+    @IBAction func onAddBtnPressed(sender: UIButton!)
+    {
+        previousResult = result
+        result = result + multipleNum
+        
+        equationTxt.text = "\(previousResult) + \(multipleNum) = \(result)"
+        
+        if areWeDone(){
+            restartGame()
+        }
+    }
+    
+    func areWeDone() -> Bool {
+        return result >= multipleNum * 11
+    }
+    func restartGame(){
+        multipleNum = 0
+        whatMultipleTxt.text = ""
+        
+        logoImg.isHidden = false
+        playBtn.isHidden = false
+        whatMultipleTxt.isHidden = false
+        
+        addBtn.isHidden = true
+        equationTxt.isHidden = true
+        
+    }
 }
 
